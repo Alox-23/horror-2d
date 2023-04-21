2a0c73fa60ce510c7bbacde122502b797afd5d92
Alox-23 <akuznetsov23000@gmail.com>
2023-04-21 20:36:30 +0200

    added collisions

--- classes/level.py
+++ classes/level.py
@@ -10,7 +10,7 @@ class level:
     def __init__(self):
         self.screan = pygame.display.set_mode((HRES, VRES))
         self.display = pygame.Surface((HRES, VRES))
-
+        self.t2 = 0
         self.clock = pygame.time.Clock()
         self.CameraGroup = sprites.CameraGroup.CameraGroup(self.display)
         self.player = sprites.player.player((HRES//2,VRES//2), self.CameraGroup)
@@ -20,24 +20,40 @@ class level:
     def run(self):
         self.get_input()
         self.display.fill((50, 50, 100))
-        self.CameraGroup.set_focal_point(self.player)
-        self.CameraGroup.update_scroll()
-        self.CameraGroup.update_rect_scroll(self.collision_rects)
-        self.CameraGroup.update()
-        self.player.collision(self.collision_rects)
+        self.dt = self.get_delta_time()
+        self.update_sprites()
+        self.update_player()
         self.draw()
+        pygame.display.set_caption(str(int(self.clock.get_fps())))
         self.screan.blit(pygame.transform.scale(self.display, (HRES, VRES)), (0,0))
         pygame.display.update()
         self.clock.tick(FPS)
 
     def get_input(self):
         pass 
-
+		
+    def get_delta_time(self):
+        t = self.clock.get_time()
+        self.dt = (t-self.t2) / 1000.0
+        self.t2 = t
+		
     def draw(self):
         self.CameraGroup.ysort_draw()
         for i in self.collision_rects:
             pygame.draw.rect(self.display, (255, 255, 255), i)
 
+    def update_CameraGroup(self):
+        self.CameraGroup.set_focal_point(self.player)
+        self.CameraGroup.update_scroll()
+        self.CameraGroup.update_rect_scroll(self.collision_rects)
+        self.CameraGroup.update()
+
+    def update_sprites(self):
+        self.update_CameraGroup()
+
+    def update_player(self):
+        self.player.collision(self.collision_rects)
+	
     def setup_level(self):
         for i in range(20):
             sprites.tree.tree((random.randint(-1500, 1500), random.randint(-1500, 1500)), self.CameraGroup)
--- sprites/player.py
+++ sprites/player.py
@@ -3,10 +3,12 @@ import pygame
 class player(pygame.sprite.Sprite):
     def __init__(self, pos, group):
         super().__init__(group)
+        self.speed = 3
+        self.coll_toll = self.speed+0.1
         self.image = pygame.image.load("img/player.png")
         self.image.set_colorkey((255, 255, 255))
         self.rect = self.image.get_rect()
-        self.speed = 3
+        self.dt = 0
         self.direction = pygame.math.Vector2()
 
         self.rect.center = pos
@@ -25,22 +27,33 @@ class player(pygame.sprite.Sprite):
             self.direction.x = 1
     
     def update(self):
-        self.input() 
+        self.input()
 
     def draw(self, screan):
         screan.blit(self.image, self.rect)
 
+    def update_dt(self, dt):
+        self.dt = dt
+		
     def collision(self, rects):
         self.rect.centerx += self.direction.x * self.speed
         self.rect.centery += self.direction.y * self.speed
         for rect in rects:   
             if self.rect.colliderect(rect):
-                if abs(self.rect.bottom - rect.top) < self.speed and self.direction.y > 0:
-                    self.rect.centery -= self.direction.y * self.speed
-                if abs(self.rect.top - rect.bottom) < self.speed and self.direction.y < 0:
-                    self.rect.centery -= self.direction.y * self.speed
-                if abs(self.rect.right - rect.left) < self.speed and self.direction.x < 0:
-                    self.rect.centerx -= self.direction.x * self.speed
-                if abs(self.rect.left - rect.right) < self.speed and self.direction.x > 0:
-                    print("ahh dady")
-                    self.rect.centerx -= self.direction.x * self.speed
+                if abs(self.rect.top - rect.bottom) < self.coll_toll:
+                    self.rect.top = rect.bottom
+                elif abs(self.rect.bottom - rect.top) < self.coll_toll:
+                    self.rect.bottom = rect.top
+                if abs(self.rect.right - rect.left) < self.coll_toll:
+                    self.rect.right = rect.left 
+                elif abs(self.rect.left - rect.right) < self.coll_toll:
+                    self.rect.left = rect.right
+                """if self.direction.y > 0 and self.direction.x > 0:
+                    self.rect.centerx += self.speed
+                if self.direction.y < 0 and self.direction.x > 0:
+                    self.rect.centery += self.speed
+                if self.direction.y < 0 and self.direction.x < 0:
+                    self.rect.centerx -= self.speed
+                if self.direction.y > 0 and self.direction.x > 0:
+                    self.rect.centery -= self.speed"""
+
